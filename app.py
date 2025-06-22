5075991dd4dd8be46f91a4184de6530bb8207cb2
mdsaad31 <mohammedsaad0462@gmail.com>
2025-06-22 20:19:10 +0530

    Ready for Render

--- app.py
+++ app.py
@@ -105,4 +105,5 @@ def get_models():
         return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
